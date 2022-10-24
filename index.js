3096762efc253c5c835b388bcea871b8768bed4d
Maru Di Primio <mariana.diprimio@gmail.com>
2022-10-24 23:27:41 +0200

    orden por mayor densidad ok

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const countriesInfoArray = fs
     flag: "r",
   })
   .split(/\r?\n/);
-console.log(countriesInfoArray)
+
 //me quedo con la primera linea del array que tiene el titulo
 const title = countriesInfoArray.shift();
 
@@ -16,14 +16,18 @@ let countriesArray = countriesInfoArray.map((country)=>{
     return country.split(' ')
 })
 
-//reconstruyo nombre del pais
+//reconstruyo el array y añado densidad de poblacion
 countriesArray = countriesArray.map((countryInfo)=>{
  let countryName = countryInfo.splice(0,countryInfo.length-2).join(' ');
  const population = parseInt(countryInfo[countryInfo.length-2].replace(/,/g,''))
  const area = parseInt(countryInfo[countryInfo.length-1].replace(/,/g,''))
  const density = parseFloat((population/area).toFixed(2))
  return [countryName,population,area,density]
- 
+})
+
+//ordeno el array segun mayor densidad de poblacion
+countriesArray = countriesArray.sort((a,b)=>{
+   return b[3]-a[3]
 })
 
 
